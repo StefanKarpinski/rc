1f3a6aeaa08f8fa382f0e65434c543a7393ff549
Stefan Karpinski <stefan.karpinski@gmail.com>
2008-11-05 15:47:11 -0800

    screenrc: customize screen; setup screen hardstatus line.

--- makelinks.sh
+++ makelinks.sh
@@ -2,6 +2,6 @@
 for x in \
   bash_profile bashrc \
   zlogin zlogout zprofile zshenv zshrc \
-  nanorc irbrc rdebugrc gitconfig
+  nanorc irbrc rdebugrc gitconfig screenrc
 do ln -sf rc/$x ~/.$x
 done
