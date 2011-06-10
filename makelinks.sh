aaf890eb68db9195bb700febec593b4e72f90768
Stefan Karpinski <stefan@karpinski.org>
2011-06-10 09:05:07 -0400

    Add ackrc file.

--- makelinks.sh
+++ makelinks.sh
@@ -2,7 +2,7 @@
 for x in \
   bash_profile bashrc \
   zlogin zlogout zprofile zshenv zshrc \
-  nanorc irbrc rdebugrc screenrc \
+  nanorc irbrc rdebugrc screenrc ackrc \
   gitconfig
 do ln -sf rc/$x ~/.$x
 done
